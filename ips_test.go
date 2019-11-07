e099c4f1af147fd17b8ff3165ca97ca2eb2adde0
Andreas Karlsson <norrland@nullbyte.se>
2019-11-07 07:06:35 +0100

    IP: Extend IP struct with more details
    Add missing attributes in the IP struct.
    Add ip/details function to get details about a specific IP.
    
    IPs: comment for IPCost
    
    New functions
    IPS: SetPTR
    IPs: Reset PTR

--- ips_test.go
+++ ips_test.go
@@ -44,6 +44,26 @@ func TestIPsIsIPv6(t *testing.T) {
 	assert.Equal(t, false, ips[3].IsIPv6(), "ip is not version 6")
 }
 
+func TestIPsDetails(t *testing.T) {
+	c := &mockClient{body: `{ "response": { "details": { "ipaddress": "127.0.0.1",
+		"netmask": "None", "broadcast": "None", "gateway": "None", "nameservers": ["127.255.255.1"],
+		"platform": "OpenVZ", "platforms": ["OpenVZ"],
+		"cost": {"amount":1, "currency": "SEK", "timeperiod": "month"},
+		"datacenter": "Falkenberg", "ipversion": 4, "serverid": "vz123456", "reserved": "yes",
+		"lockedtoaccount": "no", "ptr": "1.0.0.127-static.example.com."} } }`}
+	s := IPService{client: c}
+
+	ip, _ := s.Details(context.Background(), "127.0.0.1")
+
+	assert.Equal(t, "POST", c.lastMethod, "method used is correct")
+	assert.Equal(t, "ip/details", c.lastPath, "path used is correct")
+	assert.Equal(t, "127.0.0.1", (*ip).Address, "one ip was returned")
+	assert.Equal(t, "OpenVZ", (*ip).Platform, "platform is correct")
+	assert.Equal(t, "Falkenberg", (*ip).DataCenter, "datacenter is correct")
+	assert.Equal(t, "1.0.0.127-static.example.com.", (*ip).PTR, "ptr is correct")
+	assert.Equal(t, 1, (*ip).Cost.Amount, "cost amount is correct")
+}
+
 func TestIPsReserved(t *testing.T) {
 	c := &mockClient{body: `{ "response": { "iplist": [{ "ipaddress": "127.0.0.1"},
 		{ "ipaddress": "2001:db8::1"}] } }`}
@@ -76,4 +96,38 @@ func TestIPService_Release(t *testing.T) {
 
 	assert.Equal(t, "POST", c.lastMethod, "method used is correct")
 	assert.Equal(t, "ip/release", c.lastPath, "path used is correct")
+}
+
+func TestIPs_SetPTR(t *testing.T) {
+	c := &mockClient{body: `{ "response": { "details": { "ipaddress": "127.0.0.1",
+		"netmask": "None", "broadcast": "None", "gateway": "None", "nameservers": ["127.255.255.1"],
+		"platform": "OpenVZ", "platforms": ["OpenVZ"],
+		"cost": {"amount":1, "currency": "SEK", "timeperiod": "month"},
+		"datacenter": "Falkenberg", "ipversion": 4, "serverid": "vz123456", "reserved": "yes",
+		"lockedtoaccount": "no", "ptr": "ptr.parker.example.com."} } }`}
+	s := IPService{client: c}
+
+	ip, _ := s.SetPTR(context.Background(), "127.0.0.1", "ptr.parker.example.com.")
+
+	assert.Equal(t, "POST", c.lastMethod, "method used is correct")
+	assert.Equal(t, "ip/setptr", c.lastPath, "path used is correct")
+	assert.Equal(t, "127.0.0.1", (*ip).Address, "one ip was returned")
+	assert.Equal(t, "ptr.parker.example.com.", (*ip).PTR, "ptr is correct")
+}
+
+func TestIPs_ResetPTR(t *testing.T) {
+	c := &mockClient{body: `{ "response": { "details": { "ipaddress": "127.0.0.1",
+		"netmask": "None", "broadcast": "None", "gateway": "None", "nameservers": ["127.255.255.1"],
+		"platform": "OpenVZ", "platforms": ["OpenVZ"],
+		"cost": {"amount":1, "currency": "SEK", "timeperiod": "month"},
+		"datacenter": "Falkenberg", "ipversion": 4, "serverid": "vz123456", "reserved": "yes",
+		"lockedtoaccount": "no", "ptr": "1-0-0-127-static.glesys.net."} } }`}
+	s := IPService{client: c}
+
+	ip, _ := s.ResetPTR(context.Background(), "127.0.0.1")
+
+	assert.Equal(t, "POST", c.lastMethod, "method used is correct")
+	assert.Equal(t, "ip/resetptr", c.lastPath, "path used is correct")
+	assert.Equal(t, "127.0.0.1", (*ip).Address, "one ip was returned")
+	assert.Equal(t, "1-0-0-127-static.glesys.net.", (*ip).PTR, "ptr is correct")
 }
