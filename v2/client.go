e7677da227724f5c470cbd98afa1412b04122e85
Lars Dunemark <lars.dunemark@glesys.se>
2020-04-27 15:56:46 +0200

    Add v2 dir to work with go.mod for version above v1
    Moving all source from root to v2 dir

---
+++ v2/client.go
@@ -0,0 +1,158 @@
+package glesys
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const version = "2.4.1"
+
+type httpClientInterface interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+type clientInterface interface {
+	get(ctx context.Context, path string, v interface{}) error
+	post(ctx context.Context, path string, v interface{}, params interface{}) error
+}
+
+// Client is used to interact with the GleSYS API
+type Client struct {
+	apiKey     string
+	baseURL    *url.URL
+	httpClient httpClientInterface
+	project    string
+	userAgent  string
+
+	DNSDomains      *DNSDomainService
+	EmailDomains    *EmailDomainService
+	IPs             *IPService
+	LoadBalancers   *LoadBalancerService
+	Servers         *ServerService
+	Networks        *NetworkService
+	NetworkAdapters *NetworkAdapterService
+}
+
+// NewClient creates a new Client for interacting with the GleSYS API. This is
+// the main entrypoint for API interactions.
+func NewClient(project, apiKey, userAgent string) *Client {
+	baseURL, _ := url.Parse("https://api.glesys.com")
+
+	c := &Client{
+		apiKey:     apiKey,
+		baseURL:    baseURL,
+		httpClient: http.DefaultClient,
+		project:    project,
+		userAgent:  userAgent,
+	}
+
+	c.DNSDomains = &DNSDomainService{client: c}
+	c.EmailDomains = &EmailDomainService{client: c}
+	c.IPs = &IPService{client: c}
+	c.LoadBalancers = &LoadBalancerService{client: c}
+	c.Servers = &ServerService{client: c}
+	c.Networks = &NetworkService{client: c}
+	c.NetworkAdapters = &NetworkAdapterService{client: c}
+
+	return c
+}
+
+func (c *Client) get(ctx context.Context, path string, v interface{}) error {
+	request, err := c.newRequest(ctx, "GET", path, nil)
+	if err != nil {
+		return err
+	}
+	return c.do(request, v)
+}
+
+func (c *Client) post(ctx context.Context, path string, v interface{}, params interface{}) error {
+	request, err := c.newRequest(ctx, "POST", path, params)
+	if err != nil {
+		return err
+	}
+	return c.do(request, v)
+}
+
+func (c *Client) newRequest(ctx context.Context, method, path string, params interface{}) (*http.Request, error) {
+	u, err := url.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if c.baseURL != nil {
+		u = c.baseURL.ResolveReference(u)
+	}
+
+	buffer := new(bytes.Buffer)
+
+	if params != nil {
+		err = json.NewEncoder(buffer).Encode(params)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	request, err := http.NewRequest(method, u.String(), buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	userAgent := strings.TrimSpace(fmt.Sprintf("%s glesys-go/%s", c.userAgent, version))
+
+	request = request.WithContext(ctx)
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("User-Agent", userAgent)
+	request.SetBasicAuth(c.project, c.apiKey)
+
+	return request, nil
+}
+
+func (c *Client) do(request *http.Request, v interface{}) error {
+	response, err := c.httpClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return c.handleResponseError(response)
+	}
+
+	return c.parseResponseBody(response, v)
+}
+
+func (c *Client) handleResponseError(response *http.Response) error {
+	data := struct {
+		Response struct {
+			Status struct {
+				Text string `json:"text"`
+			} `json:"status"`
+		} `json:"response"`
+	}{}
+
+	err := c.parseResponseBody(response, &data)
+	if err != nil {
+		return err
+	}
+
+	return fmt.Errorf("Request failed with HTTP error: %v (%v)", response.StatusCode, strings.TrimSpace(data.Response.Status.Text))
+}
+
+func (c *Client) parseResponseBody(response *http.Response, v interface{}) error {
+	if v == nil {
+		return nil
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(body, v)
+}
---
+++ v2/dnsdomains.go
@@ -0,0 +1,299 @@
+package glesys
+
+import (
+	"context"
+)
+
+// DNSDomainService provides functions to interact with dns domains
+type DNSDomainService struct {
+	client clientInterface
+}
+
+//DNSDomain represents a domain
+type DNSDomain struct {
+	Name                  string           `json:"domainname"`
+	Available             bool             `json:"available,omitempty"`
+	CreateTime            string           `json:"createtime,omitempty"`
+	DisplayName           string           `json:"displayname,omitempty"`
+	Expire                int              `json:"expire,omitempty"`
+	Minimum               int              `json:"minimum,omitempty"`
+	Prices                []DNSDomainPrice `json:"prices,omitempty"`
+	PrimaryNameServer     string           `json:"primarynameserver,omitempty"`
+	RecordCount           int              `json:"recordcount,omitempty"`
+	Refresh               int              `json:"refresh,omitempty"`
+	RegistrarInfo         RegistrarInfo    `json:"registrarinfo,omitempty"`
+	ResponsiblePerson     string           `json:"responsibleperson,omitempty"`
+	Retry                 int              `json:"retry,omitempty"`
+	TTL                   int              `json:"ttl,omitempty"`
+	UsingGlesysNameserver string           `json:"usingglesysnameserver,omitempty"`
+}
+
+// DNSDomainPrice represents the price for a single domain
+type DNSDomainPrice struct {
+	Amount   int    `json:"amount"`
+	Currency string `json:"currency"`
+	Years    int    `json:"years"`
+}
+
+// AddDNSDomainParams - used for adding existing domains to GleSYS
+// use CreateRecords = false to not create additional records.
+type AddDNSDomainParams struct {
+	Name              string `json:"domainname"`
+	CreateRecords     bool   `json:"createrecords,omitempty"`
+	Expire            int    `json:"expire,omitempty"`
+	Minimum           int    `json:"minimum,omitempty"`
+	PrimaryNameServer string `json:"primarynameserver,omitempty"`
+	Refresh           int    `json:"refresh,omitempty"`
+	ResponsiblePerson string `json:"responsibleperson,omitempty"`
+	Retry             int    `json:"retry,omitempty"`
+	TTL               int    `json:"ttl,omitempty"`
+}
+
+// EditDNSDomainParams - used when editing domain parameters
+type EditDNSDomainParams struct {
+	Name              string `json:"domainname"`
+	Expire            int    `json:"expire,omitempty"`
+	Minimum           int    `json:"minimum,omitempty"`
+	PrimaryNameServer string `json:"primarynameserver,omitempty"`
+	Refresh           int    `json:"refresh,omitempty"`
+	ResponsiblePerson string `json:"responsibleperson,omitempty"`
+	Retry             int    `json:"retry,omitempty"`
+	TTL               int    `json:"ttl,omitempty"`
+}
+
+// RegistrarInfo contains information about the registrar for the domain
+type RegistrarInfo struct {
+	AutoRenew        string `json:"autorenew"`
+	State            string `json:"state"`
+	StateDescription string `json:"statedescription,omitempty"`
+	Expire           string `json:"expire,omitempty"`
+	TLD              string `json:"tld,omitempty"`
+	InvoiceNumber    string `json:"invoicenumber,omitempty"`
+}
+
+// RegisterDNSDomainParams - parameters used when registering a domain
+type RegisterDNSDomainParams struct {
+	Name               string `json:"domainname"`
+	Address            string `json:"address"`
+	City               string `json:"city"`
+	Country            string `json:"country"`
+	Email              string `json:"email"`
+	Firstname          string `json:"firstname"`
+	Lastname           string `json:"lastname"`
+	OrganizationNumber int    `json:"organizationnumber"`
+	Organization       string `json:"organization"`
+	PhoneNumber        string `json:"phonenumber"`
+	ZipCode            string `json:"zipcode"`
+
+	FaxNumber string `json:"fax,omitempty"`
+	NumYears  int    `json:"numyears,omitempty"`
+}
+
+// DeleteDNSDomainParams - parameters for deleting a domain from the dns system.
+// Set ForceDeleteEmail to true, to delete a domain AND email accounts for the domain.
+type DeleteDNSDomainParams struct {
+	Name             string `json:"domainname"`
+	ForceDeleteEmail string `json:"forcedeleteemail,omitempty"`
+}
+
+// RenewDNSDomainParams - parameters to send when renewing a domain.
+type RenewDNSDomainParams struct {
+	Name     string `json:"domainname"`
+	NumYears int    `json:"numyears"`
+}
+
+// SetAutoRenewParams - parameters to send for renewing a domain automatically.
+type SetAutoRenewParams struct {
+	Name         string `json:"domainname"`
+	SetAutoRenew string `json:"setautorenew"`
+}
+
+// DNSDomainRecord - data in the domain
+type DNSDomainRecord struct {
+	DomainName string `json:"domainname"`
+	Data       string `json:"data"`
+	Host       string `json:"host"`
+	RecordID   int    `json:"recordid"`
+	TTL        int    `json:"ttl"`
+	Type       string `json:"type"`
+}
+
+// AddRecordParams - parameters for updating domain records
+type AddRecordParams struct {
+	DomainName string `json:"domainname"`
+	Data       string `json:"data"`
+	Host       string `json:"host"`
+	Type       string `json:"type"`
+	TTL        int    `json:"ttl,omitempty"`
+}
+
+// UpdateRecordParams - parameters for updating domain records
+type UpdateRecordParams struct {
+	RecordID int    `json:"recordid"`
+	Data     string `json:"data,omitempty"`
+	Host     string `json:"host,omitempty"`
+	Type     string `json:"type,omitempty"`
+	TTL      int    `json:"ttl,omitempty"`
+}
+
+// ChangeNameserverParams - parameters for updating the nameservers for domain
+type ChangeNameserverParams struct {
+	DomainName string `json:"domainname"`
+	NS1        string `json:"NS1"`
+	NS2        string `json:"NS2"`
+	NS3        string `json:"NS3,omitempty"`
+	NS4        string `json:"NS4,omitempty"`
+}
+
+// Available - checks if the domain is available
+func (s *DNSDomainService) Available(context context.Context, search string) (*[]DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domain []DNSDomain
+		}
+	}{}
+	err := s.client.post(context, "domain/available", &data, search)
+	return &data.Response.Domain, err
+}
+
+// AddDNSDomain - add an existing domain to your GleSYS account
+func (s *DNSDomainService) AddDNSDomain(context context.Context, params AddDNSDomainParams) (*DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domain DNSDomain
+		}
+	}{}
+	err := s.client.post(context, "domain/add", &data, params)
+	return &data.Response.Domain, err
+}
+
+// Delete - deletes a domain from the dns system
+func (s *DNSDomainService) Delete(context context.Context, params DeleteDNSDomainParams) error {
+	return s.client.post(context, "domain/delete", nil, params)
+}
+
+// Details - return details about the domain
+func (s *DNSDomainService) Details(context context.Context, domainname string) (*DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domain DNSDomain
+		}
+	}{}
+	err := s.client.post(context, "domain/details", &data, struct {
+		Name string `json:"domainname"`
+	}{domainname})
+	return &data.Response.Domain, err
+}
+
+// Edit - edit domain parameters
+func (s *DNSDomainService) Edit(context context.Context, params EditDNSDomainParams) (*DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domain DNSDomain
+		}
+	}{}
+	err := s.client.post(context, "domain/edit", &data, params)
+	return &data.Response.Domain, err
+}
+
+// List - return a list of all domains in your account
+func (s *DNSDomainService) List(context context.Context) (*[]DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domains []DNSDomain
+		}
+	}{}
+	err := s.client.get(context, "domain/list", &data)
+	return &data.Response.Domains, err
+}
+
+// Register - Register a domain
+func (s *DNSDomainService) Register(context context.Context, params RegisterDNSDomainParams) (*DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domain DNSDomain
+		}
+	}{}
+	err := s.client.post(context, "domain/register", &data, params)
+	return &data.Response.Domain, err
+}
+
+// Renew - Renew a domain
+func (s *DNSDomainService) Renew(context context.Context, params RenewDNSDomainParams) (*DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domain DNSDomain
+		}
+	}{}
+	err := s.client.post(context, "domain/renew", &data, params)
+	return &data.Response.Domain, err
+}
+
+// SetAutoRenew - Set a domain to renew automatically
+func (s *DNSDomainService) SetAutoRenew(context context.Context, params SetAutoRenewParams) (*DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domain DNSDomain
+		}
+	}{}
+	err := s.client.post(context, "domain/setautorenew", &data, params)
+	return &data.Response.Domain, err
+}
+
+// Transfer - Transfer a domain
+func (s *DNSDomainService) Transfer(context context.Context, params RegisterDNSDomainParams) (*DNSDomain, error) {
+	data := struct {
+		Response struct {
+			Domain DNSDomain
+		}
+	}{}
+	err := s.client.post(context, "domain/transfer", &data, params)
+	return &data.Response.Domain, err
+}
+
+// ListRecords - return a list of all records for domain
+func (s *DNSDomainService) ListRecords(context context.Context, domainname string) (*[]DNSDomainRecord, error) {
+	data := struct {
+		Response struct {
+			Records []DNSDomainRecord
+		}
+	}{}
+	err := s.client.post(context, "domain/listrecords", &data, struct {
+		Name string `json:"domainname"`
+	}{domainname})
+	return &data.Response.Records, err
+}
+
+// AddRecord - add a domain record
+func (s *DNSDomainService) AddRecord(context context.Context, params AddRecordParams) (*DNSDomainRecord, error) {
+	data := struct {
+		Response struct {
+			Record DNSDomainRecord
+		}
+	}{}
+	err := s.client.post(context, "domain/addrecord", &data, params)
+	return &data.Response.Record, err
+}
+
+// UpdateRecord - update a domain record
+func (s *DNSDomainService) UpdateRecord(context context.Context, params UpdateRecordParams) (*DNSDomainRecord, error) {
+	data := struct {
+		Response struct {
+			Record DNSDomainRecord
+		}
+	}{}
+	err := s.client.post(context, "domain/updaterecord", &data, params)
+	return &data.Response.Record, err
+}
+
+// DeleteRecord deletes a record
+func (s *DNSDomainService) DeleteRecord(context context.Context, recordID int) error {
+	return s.client.post(context, "domain/deleterecord", nil, struct {
+		RecordID int `json:"recordid"`
+	}{recordID})
+}
+
+// ChangeNameservers - change the nameservers for domain
+func (s *DNSDomainService) ChangeNameservers(context context.Context, params ChangeNameserverParams) error {
+	return s.client.post(context, "domain/changenameservers", nil, params)
+}
