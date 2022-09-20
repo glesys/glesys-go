597a270d49c01ca1f5edc1b53f2bc620a2fea80c
Andreas Eriksson <andreas.eriksson@glesys.se>
2022-09-20 16:21:30 +0200

    fix gofmt errors

--- doc.go
+++ doc.go
@@ -3,13 +3,13 @@
 // Please note that only a subset of features available in the GleSYS API has
 // been implemented. We greatly appreciate contributions.
 //
-// Getting Started
+// # Getting Started
 //
 // To get started you need to signup for a GleSYS Cloud account and create an
 // API key. Signup is available at https://glesys.com/signup and API keys can be
 // created at https://customer.glesys.com.
 //
-//     client := glesys.NewClient("CL12345", "your-api-key", "my-application/0.0.1")
+//	client := glesys.NewClient("CL12345", "your-api-key", "my-application/0.0.1")
 //
 // CL12345 is the key of the Project you want to work with.
 //
@@ -20,11 +20,11 @@
 // The different modules of the GleSYS API are available on the client.
 // For example:
 //
-//     client.EmailDomains.Overview(...)
-//     client.IPs.List(...)
-//     client.NetworkAdapters.Create(...)
-//     client.Networks.Create(...)
-//     client.Servers.Create(...)
+//	client.EmailDomains.Overview(...)
+//	client.IPs.List(...)
+//	client.NetworkAdapters.Create(...)
+//	client.Networks.Create(...)
+//	client.Servers.Create(...)
 //
 // More examples provided below.
 package glesys
